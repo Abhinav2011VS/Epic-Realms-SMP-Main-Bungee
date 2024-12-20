f5d8315e863d1bcb3e67b5d227b9cd6733dafd32
Abhinav2011VS <abhinav2011vs@gmail.com>
2024-12-20 19:39:33 +0530

    Initial Release

---
+++ minecraft-server/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if the screen session is already running
+SESSION_NAME="minecraft"
+
+# Start the server in a new screen session if it's not already running
+if ! screen -list | grep -q "$SESSION_NAME"; then
+  screen -S $SESSION_NAME -dm bash -c "java -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -Dusing.aikars.flags=https://mcutils.com -jar server.jar --nogui"
+else
+  echo "Minecraft server is already running in screen session $SESSION_NAME."
+fi
